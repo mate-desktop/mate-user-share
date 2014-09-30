mate-desktop/mate-user-share

commit 4c6a1bdb10a2c554aedb6bb75a24ca61ef421d6d

[user-share] declare the x_fd variable as unused
Fixes a GCC warning about set but not used variables.
https://bugzilla.gnome.org/show_bug.cgi?id=676515
http://git.gnome.org/browse/gnome-user-share/commit/?id=ba5961cb3973df00a2260adc2826ec49077c2484

src/user_share.c
@@ -427,7 +427,7 @@ main (int argc, char **argv)
 {
 	GSettings *settings;
 	Display *xdisplay;
-	int x_fd;
+	G_GNUC_UNUSED x_fd;
 	Window selection_owner;
 	Atom xatom;
 

commit 4b5d33e7ee3a5bdd8d40972a1e2ee78875616dcb

Add missing variable type
Closes https://github.com/mate-desktop/mate-user-share/issues/21
Regression introduced in https://github.com/mate-desktop/mate-user-share/commit/4c6a1bdb10a2c554aedb6bb75a24ca61ef421d6d

src/user_share.c
@@ -373,7 +373,7 @@ main (int argc, char **argv)
 {
 	GSettings *settings;
 	Display *xdisplay;
-	G_GNUC_UNUSED x_fd;
+	G_GNUC_UNUSED int x_fd;
 	Window selection_owner;
 	Atom xatom;
 